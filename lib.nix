a56ea7f30635703fbb7a5b6bcface875682efdf3
Miao Zhicheng <miao@mzcholdings.com>
2023-03-25 16:54:21 +0200

    wrap under ghc-pkgs & haskell-language-server-pkgs

--- lib.nix
+++ lib.nix
@@ -1,28 +1,36 @@
 rec {
-  install = pkgs: ghcAliases : with pkgs; [
+  parseSpec = pkgs: a: let
+    a' = builtins.replaceStrings ["+hls"] [""] a;
+    useHLS = a' != a;
+    ghcVer = builtins.replaceStrings ["-" "."] ["" ""] pkgs.haskell.compiler.${a'}.haskellCompilerName;
+    vtag     = builtins.replaceStrings ["ghc"] [""] ghcVer;
+  in {
+    vtag     = vtag;
+    useHLS   = useHLS;
+    compiler = pkgs.haskell.compiler.${ghcVer};
+    packages = pkgs.haskell.packages.${ghcVer};
+  };
+
+  install = pkgs: specs : with pkgs; [
     stack
     cabal-install
     hlint
     stylish-haskell
   ] ++ (
-    let ghcs = map (a: let
-          a' = builtins.replaceStrings ["+hls"] [""] a;
-          useHLS = a' != a;
-          ghcVer = builtins.replaceStrings ["-" "."] ["" ""] haskell.compiler.${a'}.haskellCompilerName;
-          vtag     = builtins.replaceStrings ["ghc"] [""] ghcVer;
-        in  {
-          vtag     = vtag;
-          useHLS   = useHLS;
-          compiler = haskell.compiler.${ghcVer};
-          packages = haskell.packages.${ghcVer};
-        }) ghcAliases;
+    let ghcs = map (parseSpec pkgs) specs;
         ghcsWithHLS = builtins.filter (a: a.useHLS) ghcs;
-    in [(buildEnv {
-      name = "ghc-compilers";
-      ignoreCollisions = true;
-      paths = map (a: a.compiler) ghcs;
-    })] ++ (map (a:
-      a.packages.haskell-language-server
-    ) ghcsWithHLS)
-  );
+    in [
+      (buildEnv {
+        name = "ghc-pkgs";
+        ignoreCollisions = true;
+        paths = map (a: a.compiler) ghcs;
+      })
+      (buildEnv {
+        name = "haskell-language-server-pkgs";
+        ignoreCollisions = true;
+        paths = map (a:
+          a.packages.haskell-language-server
+        ) ghcsWithHLS;
+      })
+    ]);
 }
